55270b793414bbe1705a2fff0e3d92ef18f4006b
vailorx <alvarorodrigoaj@gmail.com>
2024-01-25 16:15:38 +0100

    Style: Added responsive menu buttons to play page

--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Header from '../components/Header';
 import frontPageImg from '../imagesPng/frontPageImg.png'
+import addBut from '../imagesGif/addBut.gif'
+import galeryBut from '../imagesGif/galeryBut.gif'
 export const Play = () => {
   return (
     <div className='playContainer'>
@@ -8,6 +10,16 @@ export const Play = () => {
       <div className='imageContainer'>
       <img className='frontPageImg' src={frontPageImg}></img>
       </div>
+      <div className='menuFixedButtons'>
+      <div id="fixedButton">
+        <img className='fillFixedButton' src={addBut}></img>
+      </div>
+      <div id="fixedButton">
+        <img className='fillFixedButton' src={galeryBut}></img>
+      </div>
+      </div>
+      
+      
     </div>
   )
 }
