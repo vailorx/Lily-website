a9b84e507387635f48b5222314ea5309c6c999fb
vailorx <alvarorodrigoaj@gmail.com>
2024-02-06 16:31:42 +0100

    fix: square brackets and text errors in Lily's info

--- src/components/FormContent.jsx
+++ src/components/FormContent.jsx
@@ -57,7 +57,7 @@ export const FormContent = ({classN}) => {
       Lily es como una mariposa pequeñita. A veces, las mariposas pueden tener dificultades para volar alto o para encontrar su camino.
             Lily se encuentra en esa situación en este momento. Puede sentirse un poco triste o confundida, como cuando una mariposa se enreda en una telaraña.<br></br><br></br>
             Pero aquí está la buena noticia: todos podemos ser como amigos jardineros que ayudan a las mariposas.<br></br><br></br> Podemos ser amables y cuidadosos para desenredarlas y ayudarlas a volar alto otra vez. Podemos escuchar a Lily e
-            imaginar que somos como un equipo de superhéroes. Cada uno de nosotros tiene un superpoder especial: el poder de hacer sentir bien a los demás. Así que, ¡vamos a ser el equipo que ayuda a Sofia a brillar como una estrella en el cielo!
+            imaginar que somos como un equipo de superhéroes. Cada uno de nosotros tiene un superpoder especial: el poder de hacer sentir bien a los demás. Así que... <br></br><br></br>¡vamos a ser el equipo que ayuda a Lily a brillar como una estrella en el cielo!
       </div>
     </li>
    
